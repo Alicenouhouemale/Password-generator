f763ff8957b589e5cb87fc809e714169f3245929
Alice Nouhouemale <alicenouhouemale@Alices-MacBook-Pro.local>
2022-08-27 18:09:44 -0700

    update

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-
   var userInput = window.prompt("How long do you want your password to be?")
 
-  var passwordLength = parseInt("useerInput")
-  
+  var passwordLength = parseInt(userInput)
   if (isNaN(passwordLength)) {
     window.alert("That's not a number!")
     return
-
   }
-    
-  
-}
 
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password lenght must be between 8 and 128 characters")
+    return
+  }
+
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase in your password?")
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
